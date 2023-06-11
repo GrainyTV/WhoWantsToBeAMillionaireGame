bb2a75f6f07b083e2e9014f275f9e9d145842b88
Nagy Árpád <108174632+GrainyTV@users.noreply.github.com>
2023-06-11 05:12:40 -0700

    Add files via upload

--- RemasteredGame/main.cpp
+++ RemasteredGame/main.cpp
@@ -1,9 +1,31 @@
 #include "Game.hpp"
 
+using std::exception;
+
+static void ColoredPrint(const char* string, const int& color)
+{
+	printf("\x1B[%dm%s\033[0m\n", color, string);
+}
+
 int main(int argc, char* args[])
 {
-	Game g_Game = Game::Instance();
-	g_Game.Run();
+	try
+	{
+		Game* const g_Game = Game::Instance();
+		(*g_Game).FillFileNames();
+		(*g_Game).Run();
+	}
+	catch(const exception& e)
+	{
+		// Color Value 33 = Yellow
+		ColoredPrint(e.what(), 33);
 
-	return 0;
+		// Color Value 31 = Red
+		ColoredPrint("Program exited with code: 1", 31);
+		return EXIT_FAILURE;
+	}
+
+	// Color Value 32 = Green
+	ColoredPrint("Program exited with code: 0", 32);
+	return EXIT_SUCCESS;
 }
