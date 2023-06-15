90469a52a2863c9fd77a2c578584b81c501f0ee7
GrainyTV <n.arpad@protonmail.com>
2023-06-15 20:35:32 +0200

    Custom Enum Class

--- RemasteredGame/headers/Enumeration.hpp
+++ RemasteredGame/headers/Enumeration.hpp
@@ -17,34 +17,34 @@ using std::format;
 class Enumeration
 {
 private:
-    struct EnumField
-    {
-        string name;
-        long unsigned int value;
-    };
+	struct EnumField
+	{
+		string name;
+		long unsigned int value;
+	};
 
-    deque<EnumField> enums;
+	deque<EnumField> enums;
 
-    bool Contains(const string& name) const;
-    
-    long unsigned int Find(const string& name) const;
+	bool Contains(const string& name) const;
+
+	long unsigned int Find(const string& name) const;
 
 public:
-    Enumeration();
+	Enumeration();
 
-    Enumeration(initializer_list<string> entries);
+	Enumeration(initializer_list<string> entries);
 
-    long unsigned int Count() const;
-    
-    void Add(const string& name);
-    
-    void Remove(const string& name);
-    
-    void Remove(const long unsigned int& value);
-    
-    EnumField operator[](const string& name) const;
-    
-    EnumField operator[](const long unsigned int& value) const;
+	long unsigned int Count() const;
+
+	void Add(const string& name);
+
+	void Remove(const string& name);
+
+	void Remove(const long unsigned int& value);
+
+	EnumField operator[](const string& name) const;
+
+	EnumField operator[](const long unsigned int& value) const;
 };
 
 #endif
