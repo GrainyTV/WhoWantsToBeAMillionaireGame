f40f41e645daf73326b351bb741c94269a5bebd2
GrainyTV <n.arpad@protonmail.com>
2023-06-15 20:32:23 +0200

    Custom Enum Class

---
+++ RemasteredGame/headers/Enumeration.hpp
@@ -0,0 +1,50 @@
+#ifndef ENUMERATION_HPP
+#define ENUMERATION_HPP
+
+#include <deque>
+#include <string>
+#include <stdexcept>
+#include <initializer_list>
+#include <format>
+
+using std::deque;
+using std::string;
+using std::to_string;
+using std::invalid_argument;
+using std::initializer_list;
+using std::format;
+
+class Enumeration
+{
+private:
+    struct EnumField
+    {
+        string name;
+        long unsigned int value;
+    };
+
+    deque<EnumField> enums;
+
+    bool Contains(const string& name) const;
+    
+    long unsigned int Find(const string& name) const;
+
+public:
+    Enumeration();
+
+    Enumeration(initializer_list<string> entries);
+
+    long unsigned int Count() const;
+    
+    void Add(const string& name);
+    
+    void Remove(const string& name);
+    
+    void Remove(const long unsigned int& value);
+    
+    EnumField operator[](const string& name) const;
+    
+    EnumField operator[](const long unsigned int& value) const;
+};
+
+#endif
---
+++ RemasteredGame/sources/Enumeration.cpp
@@ -0,0 +1,100 @@
+#include "Enumeration.hpp"
+
+bool Enumeration::Contains(const string& name) const
+{
+    const int MATCH = 0;
+    
+    for(int i = 0; i < enums.size(); ++i)
+    {
+        if(name.compare(enums[i].name) == MATCH)
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+long unsigned int Enumeration::Find(const string& name) const
+{
+    const int MATCH = 0;
+    
+    for(int i = 0; i < enums.size(); ++i)
+    {
+        if(name.compare(enums[i].name) == MATCH)
+        {
+            return i;
+        }
+    }
+    
+    throw invalid_argument(format("Index of element ({0:s}) cannot be found inside the enum.", name));
+}
+
+Enumeration::Enumeration()
+{
+}
+
+Enumeration::Enumeration(initializer_list<string> entries)
+{
+    for(auto entry : entries)
+    {
+        Add(entry);
+    }
+}
+
+long unsigned int Enumeration::Count() const 
+{
+    return enums.size();
+}
+
+void Enumeration::Add(const string& name)
+{
+    if(Contains(name))
+    {
+        throw invalid_argument(format("An entry with the given name ({0:s}) already exists in the enum.", name));
+    }
+    
+    enums.push_back((EnumField) { name, enums.size() });
+}
+
+void Enumeration::Remove(const string& name)
+{
+    if(enums.empty() || Contains(name) == false)
+    {
+        throw invalid_argument(format("Cannot find entry with given name ({0:s}) for deletion in the enum.", name));
+    }
+    
+    long unsigned int value = Find(name);
+    enums.erase(enums.begin() + value);
+}
+
+void Enumeration::Remove(const long unsigned int& value)
+{
+    if(enums.empty() || value >= enums.size())
+    {
+        throw invalid_argument(format("Given value ({0:d}) is either equal to or larger than the maximum ({1:d}), or you are trying to delete from empty enum.", value, enums.size()));
+    }
+    
+    enums.erase(enums.begin() + value);
+}
+
+Enumeration::EnumField Enumeration::operator[](const string& name) const
+{
+    if(enums.empty() || Contains(name) == false)
+    {
+        throw invalid_argument(format("Cannot access entry with given name ({0:s}).", name));
+    }
+    
+    long unsigned int value = Find(name);
+    return enums[value];
+}
+
+Enumeration::EnumField Enumeration::operator[](const long unsigned int& value) const
+{
+    if(enums.empty() || value >= enums.size())
+    {
+        throw invalid_argument(format("Given value ({0:d}) is either equal to or larger than the maximum ({1:d}), or you are trying to access element from empty enum.", value, enums.size()));
+    }
+    
+    return enums[value];
+}
